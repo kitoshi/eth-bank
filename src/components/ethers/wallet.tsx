682e2e779875754364b40c0d2b3dbc8227d0e5d1
kitoshi <kitoshi.charlton@gmail.com>
2022-04-26 20:42:39 -0700

    etherium balance

--- src/components/ethers/wallet.tsx
+++ src/components/ethers/wallet.tsx
@@ -1,21 +1,70 @@
-import { useEffect, useState, useCallback, EffectCallback } from 'react'
-import readBlockChain from '../../services/meta_api_call'
+import { useEffect, useState, EffectCallback } from 'react'
+import { ethers } from 'ethers'
 
 export default function Wallet() {
-  const [blockNumber, updateBlockNumber] = useState<number>(0)
+  const [blockNumber, updateBlockNumber] = useState<number>()
+  const [metaBalance, updateMetaBalance] = useState<string>()
+
+  async function readBlockChain(): Promise<number> {
+    const provider = new ethers.providers.Web3Provider(
+      window.ethereum as Window['ethereum']
+    )
+
+    // MetaMask requires requesting permission to connect users accounts
+    await provider.send('eth_requestAccounts', [])
+
+    // The MetaMask plugin also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, you need the account signer...
+    // const signer = provider.getSigner()
+
+    // Look up the current block number
+    const currentBlockNumber = await provider.getBlockNumber()
+    return currentBlockNumber
+  }
+
+  async function getBalance(): Promise<ethers.BigNumber> {
+    const provider = new ethers.providers.Web3Provider(
+      window.ethereum as Window['ethereum']
+    )
+
+    // MetaMask requires requesting permission to connect users accounts
+    const accounts = await provider.send('eth_requestAccounts', [])
+
+    // The MetaMask plugin also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, you need the account signer...
+    // const signer = provider.getSigner()
+
+    const balance = await provider.getBalance(accounts[0])
+    return balance
+  }
 
   useEffect((): ReturnType<EffectCallback> => {
     const makeBlockChainCall = async () => {
-      const number = readBlockChain()
-      return number
+      const number = Promise.resolve(readBlockChain())
+      number.then(function (number) {
+        updateBlockNumber(number)
+      })
     }
-    console.log(makeBlockChainCall())
+    makeBlockChainCall()
+  }, [])
+
+  useEffect((): ReturnType<EffectCallback> => {
+    const makeBlockChainCall = async () => {
+      const number = Promise.resolve(getBalance())
+      number.then(function (number) {
+        updateMetaBalance(ethers.utils.formatEther(number))
+      })
+    }
+    makeBlockChainCall()
   }, [])
 
   return (
     <>
       <h2>MetaMask Balance</h2>
-      <p>balance:</p>
+      <h3>Balance: {String(metaBalance)} ETH</h3>
+      <p>Current Etherium Block Number:{String(blockNumber)} </p>
     </>
   )
 }
