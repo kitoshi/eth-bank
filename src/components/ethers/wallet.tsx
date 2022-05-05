3d0239631ea1bb7da7d11b5dcc464686baad49f9
kitoshi <kitoshi.charlton@gmail.com>
2022-05-04 18:13:14 -0700

    DAI and USDC balance

--- src/components/ethers/wallet.tsx
+++ src/components/ethers/wallet.tsx
@@ -2,29 +2,11 @@ import { useEffect, useState, EffectCallback } from 'react'
 import { ethers } from 'ethers'
 
 export default function Wallet() {
-  const [blockNumber, updateBlockNumber] = useState<number>()
   const [metaBalance, updateMetaBalance] = useState<string>()
   const [daiBalance, updateDaiBalance] = useState<string>()
+  const [USDCBalance, updateUSDCBalance] = useState<string>()
   const [sendAmount, updateSendAmount] = useState<BigInt>()
   const [sendTargetAddress, updateSendTargetAddress] = useState<string>()
-
-  async function readBlockChain(): Promise<number> {
-    const provider = new ethers.providers.Web3Provider(
-      window.ethereum as Window['ethereum']
-    )
-
-    // MetaMask requires requesting permission to connect users accounts
-    await provider.send('eth_requestAccounts', [])
-
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...
-    // const signer = provider.getSigner()
-
-    // Look up the current block number
-    const currentBlockNumber = await provider.getBlockNumber()
-    return currentBlockNumber
-  }
 
   async function getBalance(): Promise<ethers.BigNumber> {
     const provider = new ethers.providers.Web3Provider(
@@ -54,21 +36,36 @@ export default function Wallet() {
       'event Transfer(address indexed from, address indexed to, uint amount)'
     ]
 
-    const daiAddress = 'dai.tokens.ethers.eth'
+    const daiAddress = '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa'
     const daiContract = new ethers.Contract(daiAddress, daiAbi, provider)
     const balance = await daiContract.balanceOf(daiAddress)
     return balance
   }
 
-  useEffect((): ReturnType<EffectCallback> => {
-    const makeBlockChainCall = async () => {
-      const number = Promise.resolve(readBlockChain())
-      number.then(function (number) {
-        updateBlockNumber(number)
-      })
-    }
-    makeBlockChainCall()
-  }, [])
+  async function getUSDCBalance(): Promise<ethers.BigNumber> {
+    const provider = new ethers.providers.Web3Provider(
+      window.ethereum as Window['ethereum']
+    )
+    const USDCAbi = [
+      // Some details about the token
+      'function name() view returns (string)',
+      'function symbol() view returns (string)',
+
+      // Get the account balance
+      'function balanceOf(address) view returns (uint)',
+
+      // Send some of your tokens to someone else
+      'function transfer(address to, uint amount)',
+
+      // An event triggered whenever anyone transfers to someone else
+      'event Transfer(address indexed from, address indexed to, uint amount)'
+    ]
+
+    const USDCAddress = '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b'
+    const USDCContract = new ethers.Contract(USDCAddress, USDCAbi, provider)
+    const balance = await USDCContract.balanceOf(USDCAddress)
+    return balance
+  }
 
   useEffect((): ReturnType<EffectCallback> => {
     const makeBlockChainCall = async () => {
@@ -80,11 +77,18 @@ export default function Wallet() {
     const makeContractCall = async () => {
       const number = Promise.resolve(getDaiBalance())
       number.then(function (number) {
-        updateDaiBalance(ethers.utils.formatUnits(number, 18))
+        updateDaiBalance(ethers.utils.formatUnits(number, 21))
+      })
+    }
+    const makeUSDCCall = async () => {
+      const number = Promise.resolve(getUSDCBalance())
+      number.then(function (number) {
+        updateUSDCBalance(ethers.utils.formatUnits(number, 13))
       })
     }
     makeBlockChainCall()
     makeContractCall()
+    makeUSDCCall()
   }, [])
 
   function handleFormSubmit(e: React.SyntheticEvent) {
@@ -116,8 +120,6 @@ export default function Wallet() {
     <>
       <h2>MetaMask Balance</h2>
       <h3>Balance: {String(metaBalance)} ETH</h3>
-      <p>Current Ethereum Block Number:{String(blockNumber)} </p>
-      <h4>Balance: {String(daiBalance)} DAI</h4>
       <p>Send Ethereum</p>
       <form id='ethereum' onSubmit={handleFormSubmit}>
         <label>Target Address</label>
@@ -128,6 +130,7 @@ export default function Wallet() {
           Submit
         </button>
       </form>
+      <h4>Balance: {String(daiBalance)} DAI</h4>
       <p>Send DAI</p>
       <form id='dai' onSubmit={handleFormSubmit}>
         <label>Target Address</label>
@@ -138,6 +141,7 @@ export default function Wallet() {
           Submit
         </button>
       </form>
+      <h4>Balance: {String(USDCBalance)} USDC</h4>
       <p>Send USDC</p>
       <form id='usdc' onSubmit={handleFormSubmit}>
         <label>Target Address</label>
