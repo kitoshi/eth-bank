346dacfad15a26a666987a3eb94f8795a5421cba
kitoshi <kitoshi.charlton@gmail.com>
2022-05-09 12:36:23 -0700

    usdc and dai transfer

--- src/components/ethers/wallet.tsx
+++ src/components/ethers/wallet.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, EffectCallback } from 'react'
+import { useEffect, useState, EffectCallback, useRef } from 'react'
 import { ethers } from 'ethers'
 
 export default function Wallet() {
@@ -7,6 +7,9 @@ export default function Wallet() {
   const [USDCBalance, updateUSDCBalance] = useState<string>()
   const [sendAmount, updateSendAmount] = useState<BigInt>()
   const [sendTargetAddress, updateSendTargetAddress] = useState<string>()
+  const ethereumForm = useRef<HTMLFormElement>(null)
+  const daiForm = useRef<HTMLFormElement>(null)
+  const usdcForm = useRef<HTMLFormElement>(null)
 
   async function getBalance(): Promise<ethers.BigNumber> {
     const provider = new ethers.providers.Web3Provider(
@@ -111,7 +114,72 @@ export default function Wallet() {
         value: ethers.utils.parseEther(amount.toString())
       })
     }
-    sendEthereumTransaction()
+    async function sendDaiTransaction() {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum as Window['ethereum']
+      )
+      const signer = provider.getSigner()
+      const daiAbi = [
+        // Some details about the token
+        'function name() view returns (string)',
+        'function symbol() view returns (string)',
+
+        // Get the account balance
+        'function balanceOf(address) view returns (uint)',
+
+        // Send some of your tokens to someone else
+        'function transfer(address to, uint amount)',
+
+        // An event triggered whenever anyone transfers to someone else
+        'event Transfer(address indexed from, address indexed to, uint amount)'
+      ]
+
+      const daiAddress = '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa'
+      const daiContract = new ethers.Contract(daiAddress, daiAbi, provider)
+      const daiWithSigner = daiContract.connect(signer)
+      await daiWithSigner.transfer(
+        address,
+        ethers.utils.parseUnits(amount.toString(), 18)
+      )
+    }
+    async function sendUSDCTransaction() {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum as Window['ethereum']
+      )
+      const signer = provider.getSigner()
+      const USDCAbi = [
+        // Some details about the token
+        'function name() view returns (string)',
+        'function symbol() view returns (string)',
+
+        // Get the account balance
+        'function balanceOf(address) view returns (uint)',
+
+        // Send some of your tokens to someone else
+        'function transfer(address to, uint amount)',
+
+        // An event triggered whenever anyone transfers to someone else
+        'event Transfer(address indexed from, address indexed to, uint amount)'
+      ]
+
+      const USDCAddress = '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b'
+      const USDCContract = new ethers.Contract(USDCAddress, USDCAbi, provider)
+      const usdcWithSigner = USDCContract.connect(signer)
+      await usdcWithSigner.transfer(
+        address,
+        ethers.utils.parseUnits(amount.toString(), 6)
+      )
+    }
+    switch (e.currentTarget.id) {
+      case 'ethereum':
+        sendEthereumTransaction()
+        break
+      case 'dai':
+        sendDaiTransaction()
+        break
+      case 'usdc':
+        sendUSDCTransaction()
+    }
 
     // etc...
   }
@@ -121,35 +189,35 @@ export default function Wallet() {
       <h2>MetaMask Balance</h2>
       <h3>Balance: {String(metaBalance)} ETH</h3>
       <p>Send Ethereum</p>
-      <form id='ethereum' onSubmit={handleFormSubmit}>
+      <form id='ethereum' onSubmit={handleFormSubmit} ref={ethereumForm}>
         <label>Target Address</label>
         <input type='text' id='address'></input>
         <label>Amount</label>
         <input type='text' id='amount'></input>
         <button type='submit' form='ethereum'>
-          Submit
+          Transfer
         </button>
       </form>
       <h4>Balance: {String(daiBalance)} DAI</h4>
       <p>Send DAI</p>
-      <form id='dai' onSubmit={handleFormSubmit}>
+      <form id='dai' onSubmit={handleFormSubmit} ref={daiForm}>
         <label>Target Address</label>
         <input type='text' id='address'></input>
         <label>Amount</label>
         <input type='text' id='amount'></input>
         <button type='submit' form='dai'>
-          Submit
+          Transfer
         </button>
       </form>
       <h4>Balance: {String(USDCBalance)} USDC</h4>
       <p>Send USDC</p>
-      <form id='usdc' onSubmit={handleFormSubmit}>
+      <form id='usdc' onSubmit={handleFormSubmit} ref={usdcForm}>
         <label>Target Address</label>
         <input type='text' id='address'></input>
         <label>Amount</label>
         <input type='text' id='amount'></input>
         <button type='submit' form='usdc'>
-          Submit
+          Transfer
         </button>
       </form>
     </>
