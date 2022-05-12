3ea744e5f5c29d49cc437bd5e259e26993ec149b
kitoshi <kitoshi.charlton@gmail.com>
2022-05-12 14:37:56 -0700

    token list render

--- src/components/ethers/currency_transactions.tsx
+++ src/components/ethers/currency_transactions.tsx
@@ -12,25 +12,46 @@ import USDCContract from '../../contracts/usdc'
 
 export default function CurrencyTransaction(props: any) {
   const [provider, setProvider] = useState<any>([])
+  const [loaded, setLoaded] = useState<boolean>(false)
 
-  async function createERC20TokenInformation(ERC20Token: string) {
+  /*  async function createERC20TokenInformation(ERC20Token: string) {
     const name = await provider[ERC20Token].name()
     const balance = provider[ERC20Token].address
     console.log(name)
     console.log(balance)
   }
-
+ */
   useEffect(() => {
     if (props.provider === undefined) {
       console.log('nothing')
+      setLoaded(false)
     } else {
       console.log('connected')
       setProvider([DaiContract(props.provider), USDCContract(props.provider)])
-      for (const item in provider) {
+      /*       for (const item in provider) {
         createERC20TokenInformation(item)
-      }
+      } */
+      setLoaded(true)
     }
   }, [props.provider])
+
+  const listItem: React.FunctionComponent = () => {
+    if (loaded === false) {
+      return (
+        <>
+          <li>loading</li>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <li key={provider[0].ERC20Token.name()}>
+            {provider[0].ERC20Token.name()}
+          </li>
+        </>
+      )
+    }
+  }
 
   /*   const ERC20TokenInformation = provider.map(
     (
@@ -50,7 +71,9 @@ export default function CurrencyTransaction(props: any) {
   ) */
   return (
     <>
-      <ul></ul>
+      <ul>
+        <li>{listItem(props)}</li>
+      </ul>
     </>
   )
 }
